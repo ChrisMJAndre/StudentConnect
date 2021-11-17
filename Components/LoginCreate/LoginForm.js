c6c3d3c7483bc184a4fd930c786c22edfe65fd2d
JohanIsak <55132272+JohanIsak@users.noreply.github.com>
2021-11-17 14:10:04 +0100

    ss

--- Components/LoginCreate/LoginForm.js
+++ Components/LoginCreate/LoginForm.js
@@ -12,8 +12,8 @@ import firebase from "firebase";
 
 // Define function and their constant that can be altered, aswell as their initial state - Chris
 function LoginForm() {
-  const [email, setEmail] = useState("christopher@andre.bz");
-  const [password, setPassword] = useState("Chris123");
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
   const [isCompleted, setCompleted] = useState(false);
   const [errorMessage, setErrorMessage] = useState(null);
 
